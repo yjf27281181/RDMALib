c48e40ad8a270144a7a4c43989650bf130caa30c
Mian Lu <mianlu@usc.edu>
2020-04-24 18:17:59 -0700

    test on cloudlab

--- nova/nova_p2_main.cpp
+++ nova/nova_p2_main.cpp
@@ -124,8 +124,8 @@ void ExampleRDMAThread::Start() {
                                                   // 40 bytes" slab class
         // Do sth with the buf.
         databuf[0] = 'L';
-        databuf[1] = 'O';
-        databuf[2] = 'L';
+        // databuf[1] = 'O';
+        // databuf[2] = 'L';
         // databuf[3] = '\0';
 
         int server_id = 1;
