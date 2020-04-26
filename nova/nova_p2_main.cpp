f0595312d666429f9111f0e2c38090b7a54c3d4c
Mian Lu <mianlu@usc.edu>
2020-04-26 01:04:53 -0700

    test on cloudlab

--- nova/nova_p2_main.cpp
+++ nova/nova_p2_main.cpp
@@ -201,7 +201,6 @@ void ExampleRDMAThread::Start() {
         sendbuf[j] = ' ';
         j++;
         // Part 3- MEM_ADDR // TODO!!!!!!!! try using memcpy()
-        /*
         stringstream ss2;
         ss2 << (uint64_t)databuf;
         ss2 >> thisPart;
@@ -209,9 +208,8 @@ void ExampleRDMAThread::Start() {
             sendbuf[j] = thisPart[i];
             j++;
         }
-        */
         // TODO!!!!!!!!!!! memcpy(); increment j accordingly
-        memcpy(sendbuf[j], (uint64_t)databuf, sizeof(uint64_t));
+        // memcpy(sendbuf[j], (uint64_t)databuf, sizeof(uint64_t));
         j += (sizeof(uint64_t)/sizeof(char));
         sendbuf[j] = ' ';
         j++;
