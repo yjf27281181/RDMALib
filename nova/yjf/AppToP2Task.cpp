2ac6c7bab6b2b1aa03f0c7417a858554a8fc58a5
juefeiyan <juefeiya@usc.edu>
2020-05-10 15:11:05 -0700

    test finish

--- nova/yjf/AppToP2Task.cpp
+++ nova/yjf/AppToP2Task.cpp
@@ -61,7 +61,7 @@ int AppToP2Task::Run()
 		//char content[] = "testContent";
 		int random = (rand() % 10);
 		RDMA_LOG(INFO) << fmt::format("random number is {}", random);
-		if(random < 10) {
+		if(random < 1) {
 			isNetworkBusy = true;
 		}
 		if ((cmd == "GET" || cmd == "HGETALL") && isNetworkBusy) {
